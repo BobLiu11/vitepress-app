d72392f4384655486ff9c98503c5be120fd16071
liubo453 <liubo453@jd.com>
2023-07-13 18:09:55 +0800

    feat: 添加配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,21 +1,114 @@
 export default {
   title: "VitePress",
+  titleTemplate: ":title-Custom Suffix",
   description: "Just playing around.",
-
+  head: [
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+  ],
+  lang: "",
+  base: "/",
+  rewrites: {
+    //'source/:page': 'destination/:page'
+  },
+  lastUpdated: true,
   themeConfig: {
+    siteTitle: '学习博客',
+    outlineTitle: '目录',
+    //logo: '/public/sun1.png',
+    editLink: {
+      pattern: 'https://github.com/BobLiu11/vitepress-app/blob/main/docs/:path'
+    },
+    // prev:true,
+    // next:true,
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
     nav: [
       { text: "vitePress", link: "/" },
-      { text: "vue", link: "./vue/index.md" },
+      { text: "vue", link: "./vue/index" },
       { text: "github", link: "https://github.com/BobLiu11/vitepress-app" },
     ],
-    // sidebar: [
-    //   {
-    //     text: "Guide",
-    //     items: [
-    //       { text: "Introduction", link: "/introduction" },
-    //       { text: "Getting Started", link: "/getting-started" },
-    //     ],
-    //   },
-    // ],
+    lastUpdated: {
+      text: "最后更新于:",
+      formatOptions: {
+        dateStyle: "full",
+        timeStyle: "medium",
+      },
+    },
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present Evan You'
+    },
+    sidebar: [
+      {
+        text: "目录",
+        items: [
+          { text: "vitePress", link: "/index" },
+          { text: "vue", link: "/vue/index" },
+        ],
+      },
+    ],
+    externalLinkIcon:true,
+    //全局搜索
+    search: {
+      provider: "local",
+    },
+    options: {
+      appId: "...",
+      apiKey: "...",
+      indexName: "...",
+      locales: {
+        zh: {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    },
+    //广告
+    // carbonAds: {
+    //   code: 'your-carbon-code',
+    //   placement: 'your-carbon-placement'
+    // },
   },
 };
--- docs/vue/index.md
+++ docs/vue/index.md
@@ -1,19 +1,19 @@
 # vue2 源码
 
-## 生命周期篇
+## 生命周期篇<Badge type="info" text="default" />
 
-### 初始化函数——initEvents
+### 初始化函数——initEvents<Badge type="tip" text="^1.9.0" />
 
 1. 父组件给子组件的注册事件中，把自定义事件传给子组件，在子组件实例化的时候进行初始化；而浏览器原生事件是在父组件中处理。实例初始化阶段调用的初始化事件函数 initEvents 实际上初始化的是父组件在模板中使用 v-on 或@注册的监听子组件内触发的事件。
 2. 浏览器原生事件是由父组件处理，而自定义事件是在子组件初始化的时候由父组件传给子组件，再由子组件注册到实例的事件系统中。
 
-### 初始化函数——initInjections
+### 初始化函数——initInjections<Badge type="warning" text="beta" />
 
 1. 允许一个祖先组件向其所有子孙后代注入一个依赖，不论组件层次有多深，并在起上下游关系成立的时间里始终生效。
 2. provide 和 inject 选项绑定的数据不是响应式的。
 3. 这里所说的数据就是我们通常所写 data、props、watch、computed 及 method，所以 inject 选项接收到注入的值有可能被以上这些数据所使用到，所以在初始化完 inject 后需要先初始化这些数据，然后才能再初始化 provide，所以在调用 initInjections 函数对 inject 初始化完之后需要先调用 initState 函数对数据进行初始化，最后再调用 initProvide 函数对 provide 进行初始化。
 
-### 初始化函数——initState
+### 初始化函数——initState<Badge type="danger" text="caution" />
 
 1. 在 Vue 组件中会写一些如 props、data、methods、computed、watch 选项，我们把这些选项称为实例的状态选项。
 2. 在函数内部初始化这 5 个选项的时候它的顺序是有意安排的，不是毫无章法的。如果你在开发中有注意到我们在 data 中可以使用 props，在 watch 中可以观察 data 和 props，之所以可以这样做，就是因为在初始化的时候遵循了这种顺序，先初始化 props，接着初始化 data，最后初始化 watch。只有按照这种顺序初始化我们才能在开发中在 data 中可以使用 props，在 watch 中可以观察 data 和 props。这 5 个选项中的所有属性最终都会被绑定到实例上，这也就是我们为什么可以使用 this.xxx 来访问任意属性。同时正是因为这一点，这 5 个选项中的所有属性名都不应该有所重复，这样会造成属性之间相互覆盖。
@@ -35,7 +35,7 @@ if (opts.watch && opts.watch !== nativeWatch) {
 }
 ```
 
-### 模版编译阶段
+### 模版编译阶段<Badge type="info">自定义</Badge>
 
 1. vue 基于源码构建的有两个版本，一个是 runtime only(一个只包含运行时的版本)，另一个是 runtime + compiler(一个同时包含编译器和运行时的完整版本)。而两个版本的区别仅在于后者包含了一个编译器。一个完整的 Vue 版本是包含编译器的，我们可以使用 template 选项进行模板编写。编译器会自动将 template 选项中的模板字符串编译成渲染函数的代码,源码中就是 render 函数。如果你需要在客户端编译模板 (比如传入一个字符串给 template 选项，或挂载到一个元素上并以其 DOM 内部的 HTML 作为模板)，就需要一个包含编译器的版本。
 
